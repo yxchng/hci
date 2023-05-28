dffb4f36557fe98993b650b63b9257f696b9016d
yxchng <yxchng@gmail.com>
2023-05-28 17:37:41 +0800

    add all pages

--- api/index.py
+++ api/index.py
@@ -18,14 +18,24 @@ def page1():
     return render_template('page1.html')
 
 @app.route('/page2')
-def page3():
+def page2():
     return render_template('page2.html')
 
 @app.route('/page3')
-def page4():
+def page3():
     return render_template('page3.html')
 
+@app.route('/page4')
+def page4():
+    return render_template('page4.html')
 
+@app.route('/page5')
+def page5():
+    return render_template('page5.html')
+
+@app.route('/page6')
+def page6():
+    return render_template('page6.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
