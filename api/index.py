6fcbf81f3e318f55f1ed5f3d4a27f88f7cf5ed51
yxchng <yxchng@gmail.com>
2023-05-28 16:53:29 +0800

    done first 3 pages

--- api/index.py
+++ api/index.py
@@ -1,11 +1,38 @@
-from flask import Flask
 
-app = Flask(__name__)
+import os
+import json
+
+import time
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__,  static_url_path='/static', template_folder='./templates')
+
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page3():
+    return render_template('page2.html')
+
+@app.route('/page3')
+def page4():
+    return render_template('page3.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
